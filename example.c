8c9f11bfa2d7a1771f2f4a107e2e618b4d70fe07
Nicolas Allemand <contact@nicolasallemand.com>
2024-02-23 14:51:16 +0100

    fix: example: reload font on SDL_RENDER_TARGETS_RESET

--- example.c
+++ example.c
@@ -73,41 +73,57 @@ int main(void) {
           screenshot(renderer, "screenshot.bmp");
         }
         break;
+      case SDL_RENDER_TARGETS_RESET: {
+        // in case of target reset, we must reload each font
+        DBGP_CloseFont(&unscii8);
+        if (DBGP_OpenFont(
+                &unscii8, renderer, DBGP_UNSCII8, sizeof(DBGP_UNSCII8),
+                DBGP_UNSCII8_WIDTH, DBGP_UNSCII8_HEIGHT) != 0) {
+          SDL_Log("Unable to initialise DBGP: %s", SDL_GetError());
+        }
+
+        DBGP_CloseFont(&unscii16);
+        if (DBGP_OpenFont(
+                &unscii16, renderer, DBGP_UNSCII16, sizeof(DBGP_UNSCII16),
+                DBGP_UNSCII16_WIDTH, DBGP_UNSCII16_HEIGHT) != 0) {
+          SDL_Log("Unable to initialise DBGP: %s", SDL_GetError());
+        }
+      } break;
       }
     }
 
     SDL_SetRenderDrawColor(renderer, 0x30, 0x30, 0x30, 0xff);
     SDL_RenderClear(renderer);
 
-    DBGP_Print(
+    DBGP_ColorPrint(
         &unscii16, renderer, 40 * 8, 0 * 16, 0x0f,
         "$00    $10    $20    $30    $40    $50    $60    $70");
-    DBGP_Print(
+    DBGP_ColorPrint(
         &unscii16, renderer, 40 * 8, 1 * 16, 0x0f,
         "$80    $90    $A0    $B0    $C0    $D0    $E0    $F0");
 
-    DBGP_Print(&unscii16, renderer, 0, 0, 0x5f, "@ SDL_DBGP! @");
-    DBGP_Print(
+    DBGP_ColorPrint(&unscii16, renderer, 0, 0, 0x5f, "@ SDL_DBGP! @");
+    DBGP_ColorPrint(
         &unscii16, renderer, 0, 1 * 16, 0x0f,
         "Color can be changed with\n"
         "$09e$0As$0Bc$0Ca$0Dp$0Ee$0F"
         " codes too.");
-    DBGP_Print(
+    DBGP_ColorPrint(
         &unscii16, renderer, 0 * 8, 3 * 16, 0x1f, "abcdefghijkl0123456789");
-    DBGP_Print(
+    DBGP_ColorPrint(
         &unscii16, renderer, 0 * 8, 4 * 16, 0x3f, "Something $F3somethinG");
 
-    DBGP_Printf(
+    DBGP_ColorPrintf(
         &unscii16, renderer, 0, 5 * 16, DBGP_DEFAULT_COLORS,
         "A string with accents: $74%s", iso_string);
 
-    DBGP_Printf(
+    DBGP_ColorPrintf(
         &unscii8, renderer, 0, 7 * 16, DBGP_DEFAULT_COLORS,
         "Default fonts include the entire ISO-8859-1 charset:");
     for (int i = 0; i < 256; i++) {
       int x = (8 * 1) + (i % 32) * DBGP_UNSCII8_WIDTH;
       int y = (8 * 16) + (i / 32) * DBGP_UNSCII8_HEIGHT;
-      DBGP_Printf(&unscii8, renderer, x, y, DBGP_DEFAULT_COLORS, "%c", i);
+      DBGP_ColorPrintf(&unscii8, renderer, x, y, DBGP_DEFAULT_COLORS, "%c", i);
     }
 
     SDL_RenderPresent(renderer);
